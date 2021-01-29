c5d04942a4494f5b48fc8b1de13b07d21698fb15
Christopher Hicks <visrule@protonmail.com>
2021-01-28 22:14:53 -0800

    Added Dice

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -22,32 +22,32 @@ class ViewController: UIViewController, ARSCNViewDelegate {
      
         sceneView.delegate = self
         
-        
-        ///   **SHAPES**                                  -Units are in *Meters*                  -How *rounded* the corners are
-        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
-        let sphere = SCNSphere(radius: 0.2)
-        
-        ///MATERIAL Container
-        let material = SCNMaterial()
-        
-        ///**Base Material Design**                                   Texture Map from -  SolarSystemScope.com
-        material.diffuse.contents = UIImage(named: "art.scnassets/8k_moon.jpg")
-        sphere.materials = [material]
-        
-        ///**Node 1** -- Position in 3-D Space
-        let node = SCNNode()
-        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
-        
-        ///ASSIGN Object to Node Position
-        node.geometry = cube
-        
-        ///ADD Node to View
-        sceneView.scene.rootNode.addChildNode(node)
+//
+//        ///   **SHAPES**                                  -Units are in *Meters*                  -How *rounded* the corners are
+//        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+//        let sphere = SCNSphere(radius: 0.2)
+//
+//        ///MATERIAL Container
+//        let material = SCNMaterial()
+//
+//        ///**Base Material Design**                                   Texture Map from -  SolarSystemScope.com
+//        material.diffuse.contents = UIImage(named: "art.scnassets/8k_moon.jpg")
+//        sphere.materials = [material]
+//
+//        ///**Node 1** -- Position in 3-D Space
+//        let node = SCNNode()
+//        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+//
+//        ///ASSIGN Object to Node Position
+//        node.geometry = cube
+//
+//        ///ADD Node to View
+//        sceneView.scene.rootNode.addChildNode(node)
         
         ///--ADD  **Light**
         sceneView.autoenablesDefaultLighting = true
         
-        
+  
         
         
         //MARK: - NEW CODE ^
@@ -59,11 +59,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
     
         
-//        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-//
-//        // Set the scene to the view
-//        sceneView.scene = scene
+        // Create a new scene
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada_copy.scn")!
+        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+            
+            //if let Becasue its An Optional
+            diceNode.position = SCNVector3(0, 0, -0.1)
+           sceneView.scene.rootNode.addChildNode(diceNode)
+        }
+        
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
